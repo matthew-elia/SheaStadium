e7ad1eb7cbf328bd894408344251ec2bc8e4f36b
Matthew Elia <matthew@Matthews-Mini.home>
2015-12-24 02:38:40 -0500

    get rid of annoying colors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,31 +6,31 @@
 		<div class="col-md-9 col-xs-12">
 			<div class="row">
 				<div class="col-md-12">
-					<div class="well" style="height:380px;background-color:cyan;">
+					<div class="well" style="height:380px;background-color:black;">
 						
 					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4 visible-lg">
-					<div class="well" style="height:300px;background-color:yellow;">
+					<div class="well" style="height:300px;background-color:black;">
 						
 					</div>
 				</div>
 				<div class="col-md-4 visible-lg">
-					<div class="well" style="height:300px;background-color:green;">
+					<div class="well" style="height:300px;background-color:black;">
 						
 					</div>
 				</div>
 				<div class="col-md-4 visible-lg">
-					<div class="well" style="height:300px;background-color:purple;">
+					<div class="well" style="height:300px;background-color:black;">
 						<!-- <iframe width="400" height="512" src="http://clocktower.org/drupal/play/12875/show/vincent-cacchione-soft-black-caged-animals?autoplay=false" frameborder="0"></iframe> -->
 					</div>
 				</div>
 			</div>
 		</div>	
 		<div class="col-md-3 col-xs-12">
-			<div class="well" style="height:700px;background-color:red;">
+			<div class="well" style="height:700px;background-color:black;">
 				<div class="page-header">
 					<h3>Search Archives</h3>
 				</div>
